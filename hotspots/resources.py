5f26a7146e0ad02a0df07b0584ed7457cc3201af
Eric So <me@eric.so>
2013-09-13 18:05:38 -0700

    adding locu api client

--- hotspots/resources.py
+++ hotspots/resources.py
@@ -1,7 +1,12 @@
+from locu.api import VenueApiClient, MenuItemApiClient
+
 class Root(object):
 	def __init__(self, request):
-		pass
+		self.venue_client = VenueApiClient('359026ce8bb4a10f302211ddea87b2b5131fa041')
 
 	def __getitem__(self, key):
 		if key == 'map':
+			venues = self.venue_client.search(locality = 'San Francisco')
+			print(venues)
+
 			return 'this is a map'
